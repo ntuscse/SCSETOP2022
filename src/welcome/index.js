abf5579d255aaca167a489f58ef65cde655331cc
Greg-Lim <limgreg33cc@gmail.com>
2022-05-30 18:19:19 +0800

    Welcome text
    responsive but not nice

--- src/welcome/index.js
+++ src/welcome/index.js
@@ -1,10 +1,29 @@
 import React from 'react'
 import './welcome.css';
 import Waves from './Waves';
+import Head from './Head';
+import Body from './BodyPara'
+import welcomeText from "./welcome.json";
+
 const Welcome = () => {
+  let headText = welcomeText.head;
+  let bodyTexts = welcomeText.body;
+
   return (
-    <div>index
-        <Waves></Waves>
+    <div class="allText" style={{display:"flex", flexDirection:"column", alignItems:"center"}}>
+      <Head
+        text={headText}
+      />
+      
+      <div class="bodyText">
+        {
+          bodyTexts.map(body=>{
+            return <Body
+              text={body}
+            />
+          })
+        }
+      </div>
 
     </div>
   )
