5ef444c12ef1b7f7907d10d2334e0231c42816e7
Greg-Lim <37796371+Greg-Lim@users.noreply.github.com>
2022-06-08 21:21:20 +0800

    Small Fix

--- src/App.js
+++ src/App.js
@@ -13,12 +13,8 @@ function App() {
       {/* uncommment your respective one to work on */}
 
       {/* <HomePage></HomePage> Ranjan */}
-<<<<<<< Updated upstream
-      <Welcome></Welcome>
-=======
       <div style={{backgroundColor:"green", height:"00px"}}></div>
       <Welcome></Welcome> {/* Greg */}
->>>>>>> Stashed changes
       <GalleryApp></GalleryApp>
       {/* <Committee></Committee> Brandon */}
     </div>
