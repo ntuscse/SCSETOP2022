57c891d07964d7928e4c6e9777c7ec5fa5e6d8c4
Brandon Yeow <brandonywl@gmail.com>
2022-06-17 15:35:16 +0800

    Fixed scaling of wave

--- src/committee/UpsideDownWaves.js
+++ src/committee/UpsideDownWaves.js
@@ -1,7 +1,7 @@
 export const UpsideDownWaves = () => {
     return (
         <div class="wave">
-            <svg width="1903" height="343" viewBox="0 0 1903 343" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <svg width="100%" height="auto" viewBox="0 0 1200 343" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none">
             <path d="M1902.7 219C1902.7 219 1348 488.5 921 232.37C494 -23.7597 0.488544 232.37 0.488544 232.37L0.488512 0.500014L1902.7 0.500012L1902.7 219Z" fill="#30595F"/>
             </svg>
         </div>
