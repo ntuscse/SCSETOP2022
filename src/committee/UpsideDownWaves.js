68196103ac5b024d6251b998834717d62a941e55
Samuel <schua045@e.ntu.edu.sg>
2022-06-27 21:46:26 +0800

    Changed background color for all components to fit style

--- src/committee/UpsideDownWaves.js
+++ src/committee/UpsideDownWaves.js
@@ -2,7 +2,7 @@ export const UpsideDownWaves = () => {
     return (
         <div class="wave">
             <svg width="100%" height="auto" viewBox="0 1 1200 343" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none">
-            <path d="M1902.7 219C1902.7 219 1348 488.5 921 232.37C494 -23.7597 0.488544 232.37 0.488544 232.37L0.488512 0.500014L1902.7 0.500012L1902.7 219Z" fill="#30595F"/>
+            <path d="M1902.7 219C1902.7 219 1348 488.5 921 232.37C494 -23.7597 0.488544 232.37 0.488544 232.37L0.488512 0.500014L1902.7 0.500012L1902.7 219Z" fill="#21333E"/>
             </svg>
         </div>
     )
