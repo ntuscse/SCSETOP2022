188ed99e27aaa35ebbc41ebbd689423dfbca4be1
Chiraag Anand <chiraagsinghanand@gmail.com>
2022-06-27 23:53:11 +0800

    updated card back

--- src/welcome2/cards/cardIndex.js
+++ src/welcome2/cards/cardIndex.js
@@ -1,123 +1,121 @@
-import	c1	from	"./AS.svg"	;
-import	c2	from	"./2S.svg"	;
-import	c3	from	"./3S.svg"	;
-import	c4	from	"./4S.svg"	;
-import	c5	from	"./5S.svg"	;
-import	c6	from	"./6S.svg"	;
-import	c7	from	"./7S.svg"	;
-import	c8	from	"./8S.svg"	;
-import	c9	from	"./9S.svg"	;
-import	c10	from	"./10S.svg"	;
-import	c11	from	"./JS.svg"	;
-import	c12	from	"./QS.svg"	;
-import	c13	from	"./KS.svg"	;
-				
-import	c14	from	"./AH.svg"	;
-import	c15	from	"./2H.svg"	;
-import	c16	from	"./3H.svg"	;
-import	c17	from	"./4H.svg"	;
-import	c18	from	"./5H.svg"	;
-import	c19	from	"./6H.svg"	;
-import	c20	from	"./7H.svg"	;
-import	c21	from	"./8H.svg"	;
-import	c22	from	"./9H.svg"	;
-import	c23	from	"./10H.svg"	;
-import	c24	from	"./JH.svg"	;
-import	c25	from	"./QH.svg"	;
-import	c26	from	"./KH.svg"	;
-				
-import	c27	from	"./AC.svg"	;
-import	c28	from	"./2C.svg"	;
-import	c29	from	"./3C.svg"	;
-import	c30	from	"./4C.svg"	;
-import	c31	from	"./5C.svg"	;
-import	c32	from	"./6C.svg"	;
-import	c33	from	"./7C.svg"	;
-import	c34	from	"./8C.svg"	;
-import	c35	from	"./9C.svg"	;
-import	c36	from	"./10C.svg"	;
-import	c37	from	"./JC.svg"	;
-import	c38	from	"./QC.svg"	;
-import	c39	from	"./KC.svg"	;
-				
-import	c40	from	"./AD.svg"	;
-import	c41	from	"./2D.svg"	;
-import	c42	from	"./3D.svg"	;
-import	c43	from	"./4D.svg"	;
-import	c44	from	"./5D.svg"	;
-import	c45	from	"./6D.svg"	;
-import	c46	from	"./7D.svg"	;
-import	c47	from	"./8D.svg"	;
-import	c48	from	"./9D.svg"	;
-import	c49	from	"./10D.svg"	;
-import	c50	from	"./JD.svg"	;
-import	c51	from	"./QD.svg"	;
-import	c52	from	"./KD.svg"	;
+import c1 from "./AS.svg";
+import c2 from "./2S.svg";
+import c3 from "./3S.svg";
+import c4 from "./4S.svg";
+import c5 from "./5S.svg";
+import c6 from "./6S.svg";
+import c7 from "./7S.svg";
+import c8 from "./8S.svg";
+import c9 from "./9S.svg";
+import c10 from "./10S.svg";
+import c11 from "./JS.svg";
+import c12 from "./QS.svg";
+import c13 from "./KS.svg";
 
-import cb1 from "./test.svg";
+import c14 from "./AH.svg";
+import c15 from "./2H.svg";
+import c16 from "./3H.svg";
+import c17 from "./4H.svg";
+import c18 from "./5H.svg";
+import c19 from "./6H.svg";
+import c20 from "./7H.svg";
+import c21 from "./8H.svg";
+import c22 from "./9H.svg";
+import c23 from "./10H.svg";
+import c24 from "./JH.svg";
+import c25 from "./QH.svg";
+import c26 from "./KH.svg";
+
+import c27 from "./AC.svg";
+import c28 from "./2C.svg";
+import c29 from "./3C.svg";
+import c30 from "./4C.svg";
+import c31 from "./5C.svg";
+import c32 from "./6C.svg";
+import c33 from "./7C.svg";
+import c34 from "./8C.svg";
+import c35 from "./9C.svg";
+import c36 from "./10C.svg";
+import c37 from "./JC.svg";
+import c38 from "./QC.svg";
+import c39 from "./KC.svg";
+
+import c40 from "./AD.svg";
+import c41 from "./2D.svg";
+import c42 from "./3D.svg";
+import c43 from "./4D.svg";
+import c44 from "./5D.svg";
+import c45 from "./6D.svg";
+import c46 from "./7D.svg";
+import c47 from "./8D.svg";
+import c48 from "./9D.svg";
+import c49 from "./10D.svg";
+import c50 from "./JD.svg";
+import c51 from "./QD.svg";
+import c52 from "./KD.svg";
+
+import cb1 from "./cardback.svg";
 
 // use https://boxy-svg.com/ to design the card using one of the working cards as a templete
 // use https://svg2jsx.com/ to convert invalid svg to jsx svg
 
-
-
 const images = [
-    c1,
-    c2,
-    c3,
-    c4,
-    c5,
-    c6,
-    c7,
-    c8,
-    c9,
-    c10,
-    c11,
-    c12,
-    c13,
+  c1,
+  c2,
+  c3,
+  c4,
+  c5,
+  c6,
+  c7,
+  c8,
+  c9,
+  c10,
+  c11,
+  c12,
+  c13,
 
-    c14,
-    c15,
-    c16,
-    c17,
-    c18,
-    c19,
-    c20,
-    c21,
-    c22,
-    c23,
-    c24,
-    c25,
-    c26,
+  c14,
+  c15,
+  c16,
+  c17,
+  c18,
+  c19,
+  c20,
+  c21,
+  c22,
+  c23,
+  c24,
+  c25,
+  c26,
 
-    c27,
-    c28,
-    c29,
-    c30,
-    c31,
-    c32,
-    c33,
-    c34,
-    c35,
-    c36,
-    c37,
-    c38,
-    c39,
+  c27,
+  c28,
+  c29,
+  c30,
+  c31,
+  c32,
+  c33,
+  c34,
+  c35,
+  c36,
+  c37,
+  c38,
+  c39,
 
-    c40,
-    c41,
-    c42,
-    c43,
-    c44,
-    c45,
-    c46,
-    c47,
-    c48,
-    c49,
-    c50,
-    c51,
-    c52,
+  c40,
+  c41,
+  c42,
+  c43,
+  c44,
+  c45,
+  c46,
+  c47,
+  c48,
+  c49,
+  c50,
+  c51,
+  c52,
 
-    cb1
-]
+  cb1,
+];
 export default images;
