0a12531d1a9f7f351b5245e40832aa957a75919d
Chiraag Anand <chiraagsinghanand@gmail.com>
2022-07-07 04:08:41 +0800

    photo page link

--- src/gallery2/components/Description.js
+++ src/gallery2/components/Description.js
@@ -1,14 +1,18 @@
-import './Description.css';
+import "./Description.css";
+import { Link } from "react-router-dom";
 
 const Description = () => {
-    return (
-        <>
-        <h2 className='photoGalTitle'>Photo Gallery</h2>
-        <p className='photoGalDesc'>Bringing TOP members, seniors and freshmen together.&nbsp;
-            <a className='vmLink' href="https://www.freecodecamp.org/">View More</a>  
-        </p>
-        </>
-    );
-}
+  return (
+    <>
+      <h2 className="photoGalTitle">Photo Gallery</h2>
+      <p className="photoGalDesc">
+        Bringing TOP members, seniors and freshmen together.&nbsp;
+        <Link to="/photos">
+          <a className="vmLink">View More</a>
+        </Link>
+      </p>
+    </>
+  );
+};
 
 export default Description;
