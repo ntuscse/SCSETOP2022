1b1de30dfda3822c6062b287a7ba020fd0c6f969
Samuel <schua045@e.ntu.edu.sg>
2022-07-08 01:21:02 +0800

    JPG to jpg issue for TOP5 committee photo

--- src/committe2/index.js
+++ src/committe2/index.js
@@ -64,7 +64,7 @@ function Committee2() {
         col1Pics={[{ path: "/CommitteePics/tech_mask.jpg", text: "Tech" }]}
         col2Pics={[{ path: "/CommitteePics/logs1_nomask.jpg", text: "Logs" }]}
         col3Pics={[{ path: "/CommitteePics/logs2_nomask.jpg", text: "Logs" }]}
-        col4Pics={[{ path: "/CommitteePics/TOP5.JPG", text: "Top 5" }]}
+        col4Pics={[{ path: "/CommitteePics/TOP5.jpg", text: "Top 5" }]}
       />
     </div>
   );
